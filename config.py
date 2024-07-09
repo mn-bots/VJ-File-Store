93956de38dfaf7a32f363e53690b761d1f737dd3
mn-bots <mohammedmusammiln@gmail.com>
2024-07-09 18:31:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ def is_enabled(value, default):
 # Ask Doubt on telegram @KingVJ01
       
 # Owner Information
-AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
+AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1002147396609 -1002116310726 -1002248136091 -1001752414571').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
 
 
 API_ID = int(environ.get("API_ID", "22980696"))
@@ -38,27 +38,27 @@ API_HASH = environ.get("API_HASH", "2b653cb53821a82097efaba6732f5d75")
 ADMINS = int(environ.get("ADMINS", "1892771262"))
 
 # Database Information
-CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://Mnclone:Mnclone@cluster0.er3noev.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-CDB_NAME = environ.get("CDB_NAME", "mnclone")
-DB_URI = environ.get("DB_URI", "mongodb+srv://Mnt:Mnt@cluster0.jaqk7ga.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = environ.get("DB_NAME", "mnt")
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://Clone:clone@cluster0.km24l1u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+CDB_NAME = environ.get("CDB_NAME", "cluster0")
+DB_URI = environ.get("DB_URI", "mongodb+srv://Man:man@cluster0.konkoyn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = environ.get("DB_NAME", "cluster0")
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
 # Bot Information
-BOT_TOKEN = environ.get("BOT_TOKEN", "6738135651:AAHD0k1Agp6GuZY3k2Zx6jC3fyEztH8jZL8")
-BOT_USERNAME = environ.get("BOT_USERNAME", "MKN_FILE_STORE_BOT") # your bot username without @
-PICS = (environ.get('PICS', 'https://graph.org/file/925fbd275ae3ce711961b.jpg https://graph.org/file/80df1f2f74e5b44620899.jpg')).split() # Bot Start Picture
+BOT_TOKEN = environ.get("BOT_TOKEN", "6417546601:AAEYk-H1EDSOnPuFJhbc-dTGuUZfeW3_Z_w")
+BOT_USERNAME = environ.get("BOT_USERNAME", "Public_file_store_mnbot") # your bot username without @
+PICS = (environ.get('PICS', 'https://graph.org/file/1a93bba7ca434a409c502.jpg')).split() # Bot Start Picture
 
 # Auto Delete Information
 AUTO_DELETE = int(environ.get("AUTO_DELETE", "5")) # Time in Minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "300")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002056291438"))
-FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1002021764807')).split()]
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002221547444"))
+FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1002230080698')).split()]
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
@@ -82,7 +82,7 @@ class Var(object):
     name = str(getenv('name', 'filetolinkvjbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '1002056291438'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002230554372'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
